1745fefaffb43448c36e8e33f83cf8c9f584f9b5
Khánh Nguyễn <nqkhanh2409@gmail.com>
2023-08-11 11:20:24 +0700

    NguyenQuocKhanh

--- quiz_app/lib/screens/quiz/components/question_card.dart
+++ quiz_app/lib/screens/quiz/components/question_card.dart
@@ -20,33 +20,35 @@ class QuestionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     QuestionController _controller = Get.put(QuestionController());
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      padding: const EdgeInsets.all(kDefaultPadding),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Column(
-        children: [
-          Text(
-            question.question,
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge
-                ?.copyWith(color: kBlackColor),
-          ),
-          const SizedBox(height: kDefaultPadding / 2),
-          ...List.generate(
-            question.options.length,
-            (index) => Option(
-              index: index,
-              text: question.options[index],
-              press: () => _controller.checkAns(question, index),
+    return ListView(children: [
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        padding: const EdgeInsets.all(kDefaultPadding),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Column(
+          children: [
+            Text(
+              question.question,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge
+                  ?.copyWith(color: kBlackColor),
             ),
-          ),
-        ],
+            const SizedBox(height: kDefaultPadding / 2),
+            ...List.generate(
+              question.options.length,
+              (index) => Option(
+                index: index,
+                text: question.options[index],
+                press: () => _controller.checkAns(question, index),
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+    ]);
   }
 }
